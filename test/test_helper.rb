6b885fd5010a9670615c8c3ab2e62577295aaf5e
Andy Young <andymyoung@gmail.com>
2014-12-30 13:59:09 -0800

    Added dynamic page titling

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# require 'growl'
+# require 'minitest/autorun'
+# require 'minitest/growl_notify'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
